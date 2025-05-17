1e1d8a2952606d3819384567f2697a323c056620
Emart99 <ezequielsimon22@yahoo.com.ar>
2025-05-17 19:15:41 -0300

    spring img

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import './sidebar.css';
 import NodeDetailsForm from './NodeDetailsForm';
 import { useCallback, useState } from "react";
 import { useOnSelectionChange } from "@xyflow/react";
-
+import SpringIcon from "../../public/spring.svg"
 
 export const sidebarComponents = [
     {
@@ -66,9 +66,11 @@ export default function Sidebar() {
                     fixed top-0 start-0 bottom-0 z-20
                     bg-white border-e border-gray-200 dark:bg-neutral-800 dark:border-neutral-700" role="dialog" tabIndex="-1" aria-label="Sidebar" >
                 <div className="relative flex flex-col h-full max-h-full ">
-                    <header className="p-4 flex justify-center items-center gap-x-2">
-                        <p className="flex-none font-semibold text-xl text-black focus:outline-hidden focus:opacity-80 p-6 dark:text-white" href="#" aria-label="Brand">
-                            Spring Microservice Initializr
+                    <header className="py-4 flex justify-center items-center">
+                        <img src={SpringIcon} className="" alt="My SVG" />
+
+                        <p className="flex-none font-semibold text-xl text-black focus:outline-hidden focus:opacity-80 p-3 dark:text-white" href="#" aria-label="Brand">
+                            Microservice Initializr
                         </p>
                     </header>
 
